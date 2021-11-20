e92eee2f615aaecc88bf54299cc941b325e944ae
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 23:23:29 +0530

    Modified for UC5 Program

--- SnakesAndLadders/SnakesAndLadders/Program.cs
+++ SnakesAndLadders/SnakesAndLadders/Program.cs
@@ -23,15 +23,33 @@ namespace SnakesAndLadders
                 {
                     case Ladder:
                         PlayerPosition = PlayerPosition + DiceRoll;
-                        Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                        //Here to prevent the player position to surpass the winning position we have to give the if condition.
+                        if (PlayerPosition > WinningPosition)
+                        {
+                            PlayerPosition = PlayerPosition - DiceRoll;
+                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                        }
+                        //Here if player reaches to 100, we have to declare him the winner.
+                        else if (PlayerPosition == WinningPosition)
+                        {
+                            Console.WriteLine("Player reached " + PlayerPosition + ". Player Won the Game");
+                            //To end the program here we are using return. 
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                        }
                         break;
                     case Snake:
                         PlayerPosition = PlayerPosition - DiceRoll;
+                        //Here we have to give the if condition so that player position doesn't goto negative value.
                         if (PlayerPosition < InitialPosition)
                         {
                             PlayerPosition = InitialPosition;
                             Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
                         }
+                        //Here if the player position is not negative player position is usually updated.
                         else
                         {
                             Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
