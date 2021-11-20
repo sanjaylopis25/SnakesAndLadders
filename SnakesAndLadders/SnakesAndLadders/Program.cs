ec9cdfe602ee87ad8b0d05d44482deacc0489021
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 23:50:56 +0530

    Modified for UC6 program

--- SnakesAndLadders/SnakesAndLadders/Program.cs
+++ SnakesAndLadders/SnakesAndLadders/Program.cs
@@ -11,11 +11,13 @@ namespace SnakesAndLadders
             const int Ladder = 1;
             const int Snake = 2;
             int PlayerPosition = InitialPosition;
-            
+            int count = 0;
             while (PlayerPosition <= WinningPosition)
             {
                 Random random = new Random();
                 int DiceRoll = random.Next(1, 7);
+                //To track the numbers of loop ran till winning, we have to assign an incremental variable within the loop
+                count++;
                 Console.WriteLine("Player got the number on dice is: " + DiceRoll);
                 Random random2 = new Random();
                 int Checkoptions = random.Next(0, 3);
@@ -33,6 +35,10 @@ namespace SnakesAndLadders
                         else if (PlayerPosition == WinningPosition)
                         {
                             Console.WriteLine("Player reached " + PlayerPosition + ". Player Won the Game");
+                            //To Count the number of times the dice was played
+                            //We have to write the incremental variable which we used to track the numbers of iterations performed by a loop.
+                            Console.WriteLine("Number of times the player rolled the dice is :" + count);
+                            Console.ReadLine();
                             //To end the program here we are using return. 
                             return;
                         }
@@ -60,7 +66,6 @@ namespace SnakesAndLadders
                         break;
                 }
             }
-            Console.ReadLine();
         }
     }
 }
