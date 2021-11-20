c5d2d5000a14b84a07e56f2eab8dd34eda853e0f
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 19:37:06 +0530

    Initial Commit

---
+++ SnakesAndLadders/SnakesAndLadders/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakesAndLadders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snakes and Ladders Game Program");
+            Console.ReadLine();
+        }
+    }
+}
