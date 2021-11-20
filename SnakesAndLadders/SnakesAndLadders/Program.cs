f679cb4d414bc07ce6cd463a8722d12c59c4425b
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 21:30:53 +0530

    Modified for UC3 program

--- SnakesAndLadders/SnakesAndLadders/Program.cs
+++ SnakesAndLadders/SnakesAndLadders/Program.cs
@@ -7,11 +7,28 @@ namespace SnakesAndLadders
         static void Main(string[] args)
         {
             const int InitialPosition = 0;
+            const int Ladder = 1;
+            const int Snake = 2;
             int PlayerPosition = InitialPosition;
-            Console.WriteLine("Single player is starting with position " + PlayerPosition);
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
             Console.WriteLine("Player got the number on dice is: " + DiceRoll);
+            Random random2 = new Random();
+            int Checkoptions = random.Next(0, 3);
+            switch (Checkoptions)
+            {
+                case Ladder:
+                    PlayerPosition = PlayerPosition + DiceRoll;
+                    Console.WriteLine("Player got Ladder. Player position is :" + PlayerPosition);
+                    break;
+                case Snake:
+                    PlayerPosition = PlayerPosition - DiceRoll;
+                    Console.WriteLine("Player got Snake. Player position is :" + PlayerPosition);
+                    break;
+                default:
+                    Console.WriteLine("Player got Nothing. Player position is :" + PlayerPosition);
+                    break;
+            }
             Console.ReadLine();
         }
     }
