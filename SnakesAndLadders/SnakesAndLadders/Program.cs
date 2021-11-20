d7327c4b5ed4d35ea65fd2924e2a5290cc3ed057
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 20:56:18 +0530

    Modified for UC2 Program

--- SnakesAndLadders/SnakesAndLadders/Program.cs
+++ SnakesAndLadders/SnakesAndLadders/Program.cs
@@ -9,6 +9,9 @@ namespace SnakesAndLadders
             const int InitialPosition = 0;
             int PlayerPosition = InitialPosition;
             Console.WriteLine("Single player is starting with position " + PlayerPosition);
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+            Console.WriteLine("Player got the number on dice is: " + DiceRoll);
             Console.ReadLine();
         }
     }
